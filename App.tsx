8e6f0642b9d772d26ccdfb843efb393d89ba776a
Eronheit <aquinoeronaldo@gmail.com>
2021-10-21 09:56:52 -0300

    chore: add fonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import {
+  useFonts, 
+  Roboto_400Regular, 
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto';
 
 import { Home } from './src/screens/Home';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular, 
+    Roboto_700Bold
+  })
+
   return (
     <Home />
   );
