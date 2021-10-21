85032339a71945866d1707622616079ddd4dc264
Eronheit <aquinoeronaldo@gmail.com>
2021-10-21 10:25:18 -0300

    feat: config StatusBar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar';
 import {
   useFonts, 
   Roboto_400Regular, 
@@ -19,6 +20,9 @@ export default function App() {
   }
 
   return (
-    <Home />
+    <>
+      <StatusBar style="light" />
+      <Home />
+    </>
   );
 }
