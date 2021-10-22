3c7e885ed2014abb52c6b4c7365f85cfef293a4d
Eronheit <aquinoeronaldo@gmail.com>
2021-10-22 09:38:34 -0300

    feat: add authentication flow

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,20 +2,25 @@ import React from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 
 import LogoSvg from '../../assets/logo.svg'
+import { useAuth } from '../hooks/auth';
 import { UserPhoto } from '../UserPhoto';
 import { styles } from './styles';
 
 export function Header() {
+  const { user, signOut } = useAuth();
+
   return(
     <View style={styles.container}>
       <LogoSvg />
 
       <View style={styles.logoutButton}>
-        <TouchableOpacity>
-          <Text style={styles.logoutText}>Sair</Text>
-        </TouchableOpacity>
+        {user && (
+          <TouchableOpacity onPress={signOut}>
+            <Text style={styles.logoutText}>Sair</Text>
+          </TouchableOpacity>
+        )}
 
-        <UserPhoto imageUri='https://github.com/eronheit.png' />
+        <UserPhoto imageUri={user?.avatar_url} />
       </View>
     </View>
   );
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,17 +2,22 @@ import React from 'react';
 import { View } from 'react-native';
 
 import { Header } from '../../components/Header';
+import { useAuth } from '../../components/hooks/auth';
 import { MessageList } from '../../components/MessageList';
 import { SendMessageForm } from '../../components/SendMessageForm';
 import { SignInBox } from '../../components/SignInBox';
 import { styles } from './styles'
 
 export function Home() {
+  const { user } = useAuth();
+
   return (
     <View style={styles.container}>
       <Header />
+
       <MessageList />
-      <SendMessageForm />
+
+      {user ? <SendMessageForm /> : <SignInBox />}
     </View>
   )
 }
